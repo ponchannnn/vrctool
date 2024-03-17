6b492a580a994e4e24abd93dd1c7a9e1c2a45988
ponCHANNN <mizuki.7302@gmail.com>
2024-03-17 13:35:35 +0900

    Initial Commit

---
+++ vrctool/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  vrctool
+//
+//  Created by 池田瑞基 on 2024/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vrctool/vrctoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  vrctoolApp.swift
+//  vrctool
+//
+//  Created by 池田瑞基 on 2024/03/17.
+//
+
+import SwiftUI
+
+@main
+struct vrctoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
