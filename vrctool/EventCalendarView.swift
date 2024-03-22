2db2e0b45699432897c0a189c5f5f75394d35b93
ponCHANNN <mizuki.7302@gmail.com>
2024-03-22 19:06:22 +0900

    ログインした後の画面遷移追加、下のtab追加

---
+++ vrctool/EventCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  EventCalendarView.swift
+//  vrctool
+//
+//  Created by 池田瑞基 on 2024/03/22.
+//
+
+import SwiftUI
+
+struct EventCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventCalendarView()
+}
---
+++ vrctool/SelectTabView.swift
@@ -0,0 +1,34 @@
+//
+//  TabView.swift
+//  vrctool
+//
+//  Created by 池田瑞基 on 2024/03/22.
+//
+
+import SwiftUI
+
+struct TabView: View {
+
+    // タブの選択項目を保持する
+    @State var selection = 1
+
+    var body: some View {
+
+        TabView(selection: $selection) {
+
+            HomeView()   // Viewファイル①
+                .tabItem {
+                    Label("Page1", systemImage: "1.circle")
+                }
+                .tag(1)
+
+            EventCalendarView()   // Viewファイル②
+                .tabItem {
+                    Label("Page2", systemImage: "2.circle")
+                }
+                .tag(2)
+
+        } // TabView ここまで
+
+    } // body
+} // View
